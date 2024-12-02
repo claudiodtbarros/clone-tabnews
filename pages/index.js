171dcff2404263b31834d3ee01da39a3f09e5dab
Claudio Daniel Tenório de Barros <claudiodtbarros@gmail.com>
2024-12-02 21:20:12 +0000

    Add lint:check and lint:fix scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Amorzinho, deixo esse recado para dizer que te amo! Se gostou, sorria! Se não gostou, dê uma cambalhota com duplo carpado para trás!</h1>
+  return (
+    <h1>
+      Amorzinho, deixo esse recado para dizer que te amo! Se gostou, sorria! Se
+      não gostou, dê uma cambalhota com duplo carpado para trás!
+    </h1>
+  );
 }
 
 export default Home;
